7511f589e1980e51b80f7b9f2f0ea5f0693ddc23
hansfrugel21 <163870073+hansfrugel21@users.noreply.github.com>
2025-03-23 13:03:48 -0400

    Update scores.js

--- api/scores.js
+++ api/scores.js
@@ -12,10 +12,11 @@ const pool = new Pool({
 
 module.exports = async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM scores' LIMIT 10);
+    const result = await pool.query('SELECT * FROM scores LIMIT 10');
     res.status(200).json(result.rows);
   } catch (err) {
     console.error('Error:', err);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
+
